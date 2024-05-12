a572b72651f7173c2def3b2bfbaf2b4f2eb080a4
Jonathan Pollyn <j.pollyn@gmail.com>
2024-05-12 06:21:49 -0700

    Update streamlit_app.py
    Code fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-impo
 import requests
 
 # Write directly to the app
