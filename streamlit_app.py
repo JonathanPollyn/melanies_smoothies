d3eb34552bae2150e3a7ca54bcf6bf1900b94e6c
Jonathan Pollyn <j.pollyn@gmail.com>
2024-05-11 21:42:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Badge 3 :umbrella_with_rain_drops:")
@@ -35,15 +36,13 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    # st.write(ingredients_list)
-    # st.text(ingredients_list)
-
+   
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-    # st.write(ingredients_string)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_dt = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -58,10 +57,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!, ' + name_on_order , icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response)
-fv_dt = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
 
  
 
