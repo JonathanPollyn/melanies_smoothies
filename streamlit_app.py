45ad8c98a5006360277c8bb67368409bd837024e
Jonathan Pollyn <j.pollyn@gmail.com>
2024-05-11 21:35:50 -0700

    Update streamlit_app.py
    Added the json to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,8 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#st.text(fruityvice_response)
+fv_dt = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
  
 
